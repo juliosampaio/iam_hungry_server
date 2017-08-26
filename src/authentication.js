4c2729263408048bc04d6393ae24821391082b0a
Julio Sampaio <juliodssampaio@gmail.com>
2017-08-26 20:38:24 -0300

    Add CORS support

--- src/authentication.js
+++ src/authentication.js
@@ -1,7 +1,7 @@
 const HEADER_REGEX = /bearer token-(.*)$/;
-const config       = require('./config'); 
+const config       = require('./config');
 
-module.exports.authenticate = async ({headers: {authorization}}, Users, jwt) => {    
+module.exports.authenticate = async ({headers: {authorization}}, Users, jwt) => {
     const token   = authorization && HEADER_REGEX.exec(authorization)[1];
     const email   = jwt.verify(token, config.secret).email;
     return token && await Users.findOne({email});
